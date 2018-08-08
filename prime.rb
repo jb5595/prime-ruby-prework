c560cfc7fa0e164a0b9c41492e1f4af53abeefa7
Jacob Berman <jacobberman1995@gmail.com>
2018-08-08 02:42:46 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,14 @@
 # Add  code here!
 def prime?(number)
-  
+  if number <= 1 
+    return false
+  elsif number<= 3
+  return true
+  elsif number % 2 == 0 || number % 3 == 0 
+  return false
+  i = 5
+  while i * i <= number
+    if number % i == || n % (i + 2) == 0 
+      return flase
+      i
 end
